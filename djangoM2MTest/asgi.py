f8aa2b5600dfd3b11d223b7da6c470df8421c6e0
SergLavrov <ls500ls@mail.ru>
2025-05-25 17:25:34 +0300

    Пример Many to Many (M2M)

---
+++ djangoM2MTest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for djangoM2MTest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoM2MTest.settings')
+
+application = get_asgi_application()
